bc9aac0ae23822d3c2adb29576f7363d02cd185a
defalt42 <gilbertb4375@gmail.com>
2021-11-23 23:33:34 -0600

    Add weather api function

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+
+import requests
 
 
 app = Flask(__name__)
 
-@app.route('/')
+def get_weather_data_by_city(city):
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid=102bac7e0c03b99c4025a5fd59aca168".format(city)
+    r = requests.get(url)
+
+    return r.json()
+
+print (get_weather_data_by_city("Austin"))
+
+@app.route('/', methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        city = request.form['city']
+        return redirect(url_for("index", weather_data = get_weather_data_by_city(city)))
     return render_template("index.html")
 
 if __name__ == '__main__':
