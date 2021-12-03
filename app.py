6262c597fd389ad03e148053bec3bb29502d8d62
defalt42 <gilbertb4375@gmail.com>
2021-12-02 22:05:54 -0600

    Integrate weather API data into index page

--- app.py
+++ app.py
@@ -6,19 +6,18 @@ import requests
 app = Flask(__name__)
 
 def get_weather_data_by_city(city):
-    url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid=102bac7e0c03b99c4025a5fd59aca168".format(city)
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=102bac7e0c03b99c4025a5fd59aca168".format(city)
     r = requests.get(url)
 
     return r.json()
-
-print (get_weather_data_by_city("Austin"))
-
+    
 @app.route('/', methods=["GET", "POST"])
 def index():
+    weather_data = None
     if request.method == "POST":
         city = request.form['city']
-        return redirect(url_for("index", weather_data = get_weather_data_by_city(city)))
-    return render_template("index.html")
+        weather_data=get_weather_data_by_city(city)
+    return render_template("index.html", weather_data=weather_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
